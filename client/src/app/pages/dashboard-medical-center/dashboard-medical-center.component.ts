abd968de0b68c137d973222408e72270960daa6f
kevinmartell91 <kevinml@Kevins-MacBook-Pro.local>
2017-11-23 22:03:22 -0500

    Added configuration of Production environemnt
    This configuration was made for testing purposes in AWS EC2 instance
    in which the web app is ready to be deployed.

--- client/src/app/pages/dashboard-medical-center/dashboard-medical-center.component.ts
+++ client/src/app/pages/dashboard-medical-center/dashboard-medical-center.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-dashboard-medical-center',
@@ -12,14 +13,25 @@ export class DashboardMedicalCenterComponent implements OnInit {
 
   medical_center_id:string;
   medical_center_name:string;
+  appVersion:string;
 
   constructor() { }
 
   ngOnInit() {
-    let currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    this.medical_center_id = currentUser && currentUser.entity._id;
-    this.medical_center_name = currentUser && currentUser.entity.name;
+    
+    this.appVersion = environment.version;
+    
+    if (environment.production) { 
 
+      let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+      this.medical_center_id = currentUser && currentUser.entity._id;
+      this.medical_center_name = currentUser && currentUser.entity.name;
+
+    } else {  
+      this.medical_center_id = environment.medical_center_id;
+      this.medical_center_name = environment.medical_center_name;
+      
+    }
   }
 
 }
