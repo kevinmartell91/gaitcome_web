dd7c14cac5e7c73a44091a87c5b36a9ef9781fce
kevinmartell91 <kevinmartell91@gmail.com>
2018-07-09 17:46:59 -0500

    Added Json Limit
    Body parse json limit was not set to its maximum size, causing an
    lenght error while sending a long string via API. The body parse
    json limit was set to 50mb. This change has a pasitive impact since
    it allows to send long strings no grater than the value mentioned
    previously.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 
 // Parsers for POST data
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // use morgan to log requests to the console
